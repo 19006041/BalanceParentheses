e06d07b1b0a0e0098b2f0d96c432037e59b489e2
Blake <19006041@vcconnect.co.za>
2021-10-14 17:14:43 +0200

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Balance
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.RichTextBox();
+            this.btnBalance = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(101, 41);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open a File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(12, 68);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(274, 283);
+            this.txtFile.TabIndex = 1;
+            this.txtFile.Text = "";
+            // 
+            // btnBalance
+            // 
+            this.btnBalance.Location = new System.Drawing.Point(12, 369);
+            this.btnBalance.Name = "btnBalance";
+            this.btnBalance.Size = new System.Drawing.Size(111, 40);
+            this.btnBalance.TabIndex = 2;
+            this.btnBalance.Text = "Check Balance";
+            this.btnBalance.UseVisualStyleBackColor = true;
+            this.btnBalance.Click += new System.EventHandler(this.btnBalance_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(309, 68);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(274, 283);
+            this.txtOutput.TabIndex = 3;
+            this.txtOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 427);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnBalance);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.RichTextBox txtFile;
+        private System.Windows.Forms.Button btnBalance;
+        private System.Windows.Forms.RichTextBox txtOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Balance
+{
+    public partial class Form1 : Form
+    {
+    
+        string FileText = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog fDialog = new OpenFileDialog();
+            fDialog.ShowDialog();
+
+            FileText = File.ReadAllText(fDialog.FileName);
+
+            txtFile.Text = FileText;
+        }
+
+        private void btnBalance_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Balance().ToString());
+        }
+
+        private bool Balance()
+        {
+            int i = 0;
+            txtOutput.Text = String.Empty;
+
+            Stack<char> stack = new Stack<char>();
+
+            //Checking if the text contains parentheses
+            if (FileText.Contains('{') || FileText.Contains('(') || FileText.Contains('[') || FileText.Contains(']') || FileText.Contains(')') || FileText.Contains('}'))
+            {
+                //loop through each character
+                foreach (char c in FileText)
+                {
+                    //pushing open brackets to the stack
+                    switch (c)
+                    {
+                        case '{': stack.Push(c); txtOutput.AppendText($"\n Pushed {c} to stack"); break;
+                        case '(': stack.Push(c); txtOutput.AppendText($"\n Pushed {c} to stack"); break;
+                        case '[': stack.Push(c); txtOutput.AppendText($"\n Pushed {c} to stack"); break;
+
+                        default:
+                            break;
+                    }
+
+                    //popping off from the stack when a closing bracket is encountered
+                    switch (c)
+                    {
+                        case '}':
+                            if (stack.IsEmpty())
+                            {
+                                txtOutput.AppendText($"{c} - Stack is Empty");
+                                return false;
+                            }
+                            else
+                            {
+                                if (stack.Pop() != '{')
+                                {
+                                    txtOutput.AppendText($"\n{c} - No match - cb  i = {i}");
+                                    txtOutput.AppendText($"\n Bad chars - {FileText.Substring(i, 50)}  /{c}/");
+                                    return false;
+                                }
+                                else
+                                {
+                                    txtOutput.AppendText($"\n Pop Successful {c} ");
+                                }
+                                break;
+                            }
+
+                        case ')':
+                            if (stack.IsEmpty())
+                            {
+                                txtOutput.AppendText($"{c} - Stack  is Empty");
+                                return false;
+                            }
+                            else
+                            {
+                                if (stack.Pop() != '(')
+                                {
+                                    txtOutput.AppendText($"\n{c} - No match - b i = {i}");
+                                    txtOutput.AppendText($"\n Bad chars - {FileText.Substring(i, 50)}  /{c}/");
+                                    return false;
+                                }
+                                else
+                                {
+                                    txtOutput.AppendText($"\n Pop Successful {c} ");
+                                }
+                                break;
+                            }
+
+                        case ']':
+                            if (stack.IsEmpty())
+                            {
+                                txtOutput.AppendText($"{c} - Stack is Emptyl");
+                                return false;
+                            }
+                            else
+                            {
+                                if (stack.Pop() != '[')
+                                {
+                                    txtOutput.AppendText($"\n{c} - No match - sb i = {i}");
+                                    txtOutput.AppendText($"\n Bad chars - {FileText.Substring(i, 50)}  /{c}/");
+                                    return false;
+                                }
+                                else
+                                {
+                                    txtOutput.AppendText($"\n Pop Successful {c} ");
+                                }
+                                break;
+                            }
+
+                        default:
+                            break;
+                    }
+                    i++;
+                }
+
+                bool result = stack.IsEmpty();
+
+                if (!result)
+                {
+                    txtOutput.AppendText("\nStack was not empty");
+                }
+
+                return result;
+
+            }
+            else
+            {
+                //Returning false if there are no parentheses in the file.
+                return false;
+            }
+        }
+    }
+}
